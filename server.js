a970168919f8d3c964254400fcf415e7fb19e22d
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-02-28 18:44:29 +0200

    Handling asynchronous methods using async/await technique

--- server.js
+++ server.js
@@ -33,11 +33,8 @@ app.get('/api/stock-prices', async (req, res) => {
     stock = [stock];
   }
 
-  let closedStock;
-  let symbolUpper;
-
-  const promises = stock.map((stockIter) => {
-    return axios.get(`${URL}&symbol=${stockIter}&apikey=${API_KEY}`).then((response) => {
+  const promises = stock.map(async (stockIter) => {
+    const response = await axios.get(`${URL}&symbol=${stockIter}&apikey=${API_KEY}`);
 
       if(response.data['Note']){
         return { note: response.data['Note'] };
@@ -49,32 +46,33 @@ app.get('/api/stock-prices', async (req, res) => {
 
       const metadataObj = response.data['Meta Data'];
       const symbol = metadataObj['2. Symbol'];
-      symbolUpper = symbol.toUpperCase();
+      const symbolUpper = symbol.toUpperCase();
       const timeSeries = response.data['Time Series (Daily)'];
       const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
       const firstObj = timeSeries[yesterday];
 
-      closedStock = firstObj['4. close'];
+      let closedStock = firstObj['4. close'];
       closedStock = parseFloat(closedStock).toFixed(2);
 
-      return Stock.findOne({ stock: symbolUpper });
-    }).then((stockDb) => {
+      let stockDb = await Stock.findOne({ stock: symbolUpper });
+
       if(stockDb && (stockDb.note || stockDb.error))return stockDb;
 
+      let newStockDb;
+
       if(!stockDb){
-        const newStockDb = new Stock({ stock: symbolUpper, price: closedStock });
-        return newStockDb.save();
+        newStockDb = new Stock({ stock: symbolUpper, price: closedStock });
+        newStockDb = await newStockDb.save();
+        return newStockDb;
       }
 
       if(stockDb.price !== closedStock){
         stockDb.price = closedStock;
-        return stockDb.save();
+        stockDb = stockDb.save();
+        return stockDb
       }
 
       return stockDb;
-    }).then((stockDb2) => {
-      return stockDb2;
-    });
   });
 
   return Promise.all(promises).then((results) => {
